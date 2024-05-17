166d3d4fd0437914b4e126081312c7fffedef15f
Pravesh Bhandari <02230270.cst@rub.edu.bt>
2024-05-17 13:43:15 +0530

    chanhes to inheritence.py

--- function_excercise.py
+++ function_excercise.py
@@ -3,15 +3,9 @@ def reverse_string(s):
     if len(s) <= 1:
         return s
     else:
-        
-        first_char = s[0]
-        remaining_chars = s[1:]
-        
-        reversed_remaining = reverse_string(remaining_chars)
-        
-        reversed_string = reversed_remaining + first_char
-        return reversed_string
 
+        return reverse_string(s[1:])+s[0]
+    
+print(reverse_string("Hello"))
 
-print(reverse_string("hello"))  
       
